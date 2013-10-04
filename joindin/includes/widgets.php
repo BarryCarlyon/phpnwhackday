233b035d6af2ba4188515fcdf0046eb70b9a7b62
Barry Carlyon <barry@barrycarlyon.co.uk>
2013-10-04 21:53:18 +0100

    if I could fucking spell

--- joindin/includes/widgets.php
+++ joindin/includes/widgets.php
@@ -12,9 +12,35 @@ class Joind_In_Widget extends WP_Widget {
     function widget($args, $instance) {
         extract($args);
 
+        if (!$event_id) {
+            return;
+        }
+
         echo $before_widget;
         echo '<h2>Joind In</h2>';
-        echo $after_widget;[]
+        echo $after_widget;
+    }
+
+
+    function update($new, $old) {
+        $instance = $old;
+        $new = wp_parse_args( (array) $new, array(
+            'event_id' => 0,
+        ));
+        $new['event_id'] = $new['event_id'];
+
+        return $new;
+    }
+    function form ($instace) {
+        $instance = wp_parse_args( (array) $instance, array(
+            'event_id' => 0,
+        ));
+
+        ?>
+        <p><label for="event_id">Event ID</label>
+            <input type="text" id="event_id" name="event_id" value="<?php echo $instance['event_id']; ?>" />
+        </p>
+        <?php
     }
 }
 register_widget('Joind_In_Widget');
